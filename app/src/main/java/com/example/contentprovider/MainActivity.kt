b83fc20f98ca21c3fd30f2153327744a678fb523
Tehleel Mir <tehleelmir@Tehleels-MacBook-Pro.local>
2022-05-30 09:29:00 +0530

    done

--- app/src/main/java/com/example/contentprovider/MainActivity.kt
+++ app/src/main/java/com/example/contentprovider/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.contentprovider
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        if ( checkReadContactPermission() )
+            readContacts()
+    }
+
+    private fun checkReadContactPermission(): Boolean {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 100)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 100 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            readContacts()
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun readContacts() {
+        val uri: Uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        val cursor = contentResolver.query(uri, null, null, null, null)
+
+        if(cursor != null && cursor.count > 0)
+            while(cursor.moveToNext())
+                Log.i("here22",
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    )
     }
 }
